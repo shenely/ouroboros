ac4ebb0d4ae93013a45c407e8cefe69be3eac03a
shenely <sean.henely@gmail.com>
2018-04-19 15:04:08 -0400

    Solidifying structure of my mnemonics idea

--- doc/tlm.js
+++ doc/tlm.js
@@ -1,12 +1,3 @@
-$Type
-"cmd|raw|lin|pow|exp|log|poly|enum|mask|date|text|func"
-
-$Color
-{ $red:   Number, //int
-  $green: Number, //int
-  $blue:  Number, //int
-  $alpha: Number }//int
-
 $Datetime
 { $date: Number, //int
   $time: Number }//float
@@ -14,92 +5,133 @@ $Datetime
 $Period
 { $period: Number }//float
 
+$Color
+{ $red:   Number, //int
+  $green: Number, //int
+  $blue:  Number, //int
+  $alpha: Number }//int
+
+$Scale
+[ { _id: ObjectId,
+    $name: String,
+    $size: Number,//int
+    $type: String,
+    ... },
+  { ...,//$Scale:raw
+    $type: "raw" },
+  { ...,//$Scale:lin|pow|log
+    $type: "lin|pow|log",
+    $lower: Number,  //float
+    $upper: Number },//float
+  { ...,//$Scale:exp
+    $type: "exp",
+    $base: Number,  //float
+    $rate: Number },//float
+  { ...,//$Scale:poly
+    $type: "poly",
+    $coeff: [ Number, ... ] },//float
+  { ...,//$Scale:enum|mask
+    $type: "enum|mask",
+    $map: [ { $key: Number,//int
+              $value: String }, ... ] },
+  { ...,//$Scale:date
+    $type: "date",
+    $epoch: Object,  //$Datetime
+    $scale: Object },//$Period
+  { ...,//$Scale:func
+    $type: "func",
+    $script: String,
+    $args: [ ObjectId, ... ] } ]//$Mneumonic|tlm._id
+
 $Level
-[ { $name: String,
+[ { _id: ObjectId,
+    $name: String,
+    $type: String,
     $color: Object,//$Color
     $value: Number,//int
     ... },
-  { ... },//Level:null
+  { ...,//Level:null
+    $type: null },
   { ...,//Level:int
+    $type: "int",
     $lower: Number,  //int
     $upper: Number },//int
   { ...,//Level:float
+    $type: "float",
     $lower: Number,  //float
     $upper: Number },//float
   { ...,//Level:date
+    $type: "date",
     $lower: Object,  //$Period
     $upper: Object },//$Period
   { ...,//Level:map
+    $type: "map",
     $keys: [ Number, ... ] } ]//int
        
 $Packet
-{ $data: [ Number, ... ] }//int
+{ _id: ObjectId,
+  $data: [ Number, ... ] }//int
 
 $Command
-{ $key: Number,//int
+{ _id: ObjectId,
+  $???: ObjectId,//$Mnemonic:cmd._id
+  $epoch: Object,//$Datetime
   $values: [ 
     Number | String | Object,//$Datetime
     ...
-  ] }
-
-$Telemetry
-{ $key: Number,//int
-  $value: Number | String | Object,//$Datetime
-  $epoch: Object,//$Datetime
+  ],
   $words: [ String, ... ],
   $color: Object }//$Color
 
+$Telemetry
+{ _id: ObjectId,
+  $???: ObjectId,//$Mnemonic:tlm._id
+  $epoch: Object,//$Datetime
+  $value: Number | String | Object,//$Datetime
+  $words: [ String, ... ],
+  $color: Object }//$Color
+
+$Table
+{ _id: ObjectId,
+  $???: ObjectId,//$Mnemonic:table._id
+  $head: { 
+    Number | String | Object,//$Datetime
+    ...
+  },
+  $body: [ { 
+    String: Number | String | Object,//$Datetime
+    ... }, ... ],
+  $tail: { 
+    Number | String | Object,//$Datetime
+    ...
+  } }
+
 $Mnemonic
-[ { $key: Number,//int
+[ { _id: ObjectId,
     $name: String,
-    $size: Number,//int
-    $type: String,//$Type
+    $key: Number,//int
+    $type: String,
     ... },
+  { ...,//$Mnemonic:null
+    $key: null,
+    $type: null,
+    $scale: ObjectId },//$Scale._id
   { ...,//$Mnemonic:cmd
     $type: "cmd",
-    $conf: [ Number, ... ],//int
-    $level: Object },//$Level:str
-  { ...,//$Mnemonic:raw
-    $type: "raw",
-    $config: null,
-    $levels: [ Object, ... ] },//$Level:int
-  { ...,//$Mnemonic:lin|pow|log
-    $type: "lin|pow|log",
-    $config: { $lower: Number,  //float
-               $upper: Number },//float
-    $levels: [ Object, ... ] },//$Level:float
-  { ...,//$Mnemonic:exp
-    $type: "exp",
-    $config: { $base: Number,  //float
-               $rate: Number },//float
-    $levels: [ Object, ... ] },//$Level:float
-  { ...,//$Mnemonic:poly
-    $type: "poly",
-    $config: [ Number, ... ],//float
-    $levels: [ Object, ... ] },//$Level:float
-  { ...,//$Mnemonic:enum
-    $type: "enum",
-    $config: [ { $key: Number,//int
-                 $value: String }, ... ],
-    $levels: [ Object, ... ] },//$Level:map
-  { ...,//$Mnemonic:mask
-    $type: "mask",
-    $conf: [ { $key: Number,//int
-               $value: String }, ... ],
-    $levels: [ Object, ... ] },//$Level:map
-  { ...,//$Mnemonic:date
-    $type: "date",
-    $config: { $epoch: Object,  //$Datetime
-               $scale: Object },//$Period
-    $levels: [ Object, ... ] },//$Level:date
-  { ...,//$Mnemonic:text
-    $size: null,
-    $type: "text",
-    $config: { $format: String,
-               $keys: [ Number, ... ] },//int
-    $level: Object } ,//$Level:null
-  { ...,//$Mnemonic:func
-    $size: null,
-    $type: "calc",
-    $config: [ Number, ... ],//int
-    $levels: [ Object, ... ] } ]//$Level
+    $values: [ ObjectId, ... ],//$Mneumonic:null._id
+    $level: ObjectId },//$Level:null._id
+  { ...,//$Mnemonic:tlm
+    $type: "tlm",
+    $scale: ObjectId,//$Scale._id
+    $levels: [ ObjectId, ... ] },//$Level._id
+  { ...,//$Mnemonic:msg
+    $type: "msg",
+    $format: String,//printf
+    $values: [ ObjectId, ... ] },//$Mnemonic._id
+    $level: ObjectId } ,//$Level:null._id
+  { ...,//$Mnemonic:table
+    $type: "table",
+    $head: [ ObjectId, ... ],//$Mnemonic._id
+    $body: { $rows: Number,//int
+             $cols: [ ObjectId, ... ] },//$Mnemonic._id
+    $tail: [ ObjectId, ... ] } ]//$Mnemonic._id
