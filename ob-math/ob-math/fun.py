2f8beb872ef2e44fd8c08d4031f9036401050e06
shenely <sean.henely@gmail.com>
2018-07-30 21:58:11 -0700

    Added simple math function

---
+++ ob-math/ob-math/fun.py
@@ -0,0 +1,61 @@
+#built-in libraries
+import math
+import random
+
+#external libraries
+#...
+
+#internal libraries
+from ouroboros import NORMAL, Item, PROCESS
+
+#exports
+__all__ = ('inc', 'dec', 'rand')
+
+#constants
+#...
+
+@PROCESS('fun.inc', NORMAL,
+         Item('usr',
+              evs=('inc',), args=(),
+              ins=(), reqs=('x',),
+              outs=(), pros=('x',)))
+def inc(usr):
+    """Increment"""    
+    right = yield
+    while True:
+        usr = right['usr']
+        (value,), _ = usr.next()
+        value += 1
+        usr = (((value,), ()),)
+        left = {'usr': usr}
+        right = yield left
+
+@PROCESS('fun.dec', NORMAL,
+         Item('usr',
+              evs=('dec',), args=(),
+              ins=(), reqs=('x',),
+              outs=(), pros=('x',)))
+def dec(usr):
+    """Decrement"""    
+    right = yield
+    while True:
+        usr = right['usr']
+        (value,), _ = usr.next()
+        value += 1
+        usr = (((value,), ()),)
+        left = {'usr': usr}
+        right = yield left
+
+@PROCESS('fun.rand', NORMAL,
+         Item('usr',
+              evs=('rand',), args=(),
+              ins=(), reqs=(),
+              outs=(), pros=('x',)))
+def rand(usr):
+    """Random"""    
+    yield
+    while True:
+        value = random.random()
+        usr = (((value,), ()),)
+        left = {'usr': usr}
+        yield left
